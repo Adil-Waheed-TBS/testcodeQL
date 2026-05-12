d615520fa713bef21854a6058ad57a86c30d0af4
Adil Waheed <Adil.Waheed@tbsit360.com>
2026-05-12 18:32:36 +0500

    initial commit

---
+++ src/main/java/com/example/testcodeQL/TestcodeQlApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcodeQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcodeQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcodeQlApplication.class, args);
+	}
+
+}
